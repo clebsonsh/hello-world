786a131b1206c7ead9f3614f30593fe8df040f77
Clebson Moura <clebson.sh@gmail.com>
2024-01-16 14:13:13 -0300

    Add Sum function to calculate the sum of an array

---
+++ arraysandslices/sum.go
@@ -0,0 +1,10 @@
+package arraysandslices
+
+// Sum takes an array of numbers and returns the sum of them
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
