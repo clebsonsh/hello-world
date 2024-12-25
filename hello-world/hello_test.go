fc5351eac9212aadbda881b7fe3a1ed25a2f3598
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 08:30:27 -0300

    test: create a test group and test if we have a default value for name

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Clebson")
-	want := "Hello, Clebson"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Clebson")
+		want := "Hello, Clebson"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
