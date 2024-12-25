c70b5f40357e29e1dfcc25fd1b45f4303c432621
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 09:05:48 -0300

    refactor: give a empty string when we want the default language

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,13 +4,14 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Clebson")
+		got := Hello("Clebson", "")
 		want := "Hello, Clebson"
 
 		assertCorrectMessage(t, got, want)
 	})
+
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
