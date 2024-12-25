aa89bb92372f19d310aa09699b3547b0bcd6ae1a
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 19:35:39 -0300

    test: add test to SumAllTails slices funcionality

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -36,6 +36,22 @@ func TestSumAll(t *testing.T) {
 	checkSums(t, got, want)
 }
 
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
+
 func checkSums(t testing.TB, got, want []int) {
 	t.Helper()
 
