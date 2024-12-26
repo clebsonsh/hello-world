9d457dde2c904d157c53e09f85f732e5a6f5fa2f
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 22:05:48 -0300

    feat: implemnts the Retangle Area funcionality

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,8 +1,17 @@
 package structs
 
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 func Perimeter(rectangle Rectangle) float64 {
