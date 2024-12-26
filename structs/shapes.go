99a6a04d0e091254015043105ee347dba64fac4c
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 22:07:25 -0300

    feat: implemnts the Triangle Area funcionality

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,5 +1,6 @@
 package structs
 
+import "math"
 
 type Shape interface {
 	Area() float64
@@ -20,6 +21,15 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
 
 func Perimeter(rectangle Rectangle) float64 {
