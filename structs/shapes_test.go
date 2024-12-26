fb1b2d35973850d29e20924722e4c15aed04cb0b
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 22:01:31 -0300

    test: add test for the Retangle Area funcionality

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -11,3 +11,23 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape   Shape
+		name    string
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 5}, hasArea: 60.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
+}
