d760513858f6dd0de192da5a32d71e9b9a93abe6
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 22:00:18 -0300

    feat: implements the Perimeter funcionality

---
+++ structs/shapes.go
@@ -0,0 +1,10 @@
+package structs
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
