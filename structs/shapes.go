50e63502ad9c8b76b1ffe3f2c8d62281d0f51af6
Clebson Moura <clebson.sh@gmail.com>
2024-12-25 22:06:39 -0300

    feat: implemnts the Circle Area funcionality

--- structs/shapes.go
+++ structs/shapes.go
@@ -14,6 +14,14 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
