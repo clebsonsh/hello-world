dba6cefeda4ebd0b86592d833b133be2fc71a8b4
Clebson Moura <clebson.sh@gmail.com>
2024-12-28 20:45:20 -0300

    test: add case for search a know wokd in dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -2,6 +2,17 @@ package maps
 
 import "testing"
 
+func TestSearch(t *testing.T) {
+	dictonary := Dictionary{"test": "this is just a test"}
+
+	t.Run("know word", func(t *testing.T) {
+		got, _ := dictonary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+}
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
