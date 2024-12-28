02727fc5c194f17eae2c1bd844105feb27b9b201
Clebson Moura <clebson.sh@gmail.com>
2024-12-28 18:50:44 -0300

    test: add helpers for TestWallet

---
+++ pointers/wallet_test.go
@@ -0,0 +1,33 @@
+package pointers
+
+import (
+	"testing"
+)
+
+func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
+	t.Helper()
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
