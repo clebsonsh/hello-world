cb66b17e8453df39381b54ba85cb64f567a6c31c
Clebson Moura <clebson.sh@gmail.com>
2024-12-28 20:46:54 -0300

    feat: implements dictionary search method

---
+++ maps/dictionary.go
@@ -0,0 +1,22 @@
+package maps
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
