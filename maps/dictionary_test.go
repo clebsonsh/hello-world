c20ccdc49901a9700e3e75bedd62d55ac1c07d88
Clebson Moura <clebson.sh@gmail.com>
2024-12-28 20:44:15 -0300

    test: add helpers for dictionary tests

---
+++ maps/dictionary_test.go
@@ -0,0 +1,19 @@
+package maps
+
+import "testing"
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
